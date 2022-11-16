66910e29ebb489b949ecadcac275286e54dbe680
Jordan Slater <slatjord@gmail.com>
2022-11-15 23:17:30 -0500

    Added calibration header in data

---
+++ include/calibration.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace Calibration {
+    /*
+    -Full-Scale Range
+    -AFS_SEL=0 ±2 g
+    -AFS_SEL=1 ±4 g
+    -AFS_SEL=2 ±8 g
+    -AFS_SEL=3 ±16 g
+    -Sensitivity Scale Factor
+    -AFS_SEL=0 16,384 LSB/g
+    -AFS_SEL=1 8,192 LSB/g
+    -AFS_SEL=2 4,096 LSB/g
+    -AFS_SEL=3 2,048 LSB/g
+    */
+    struct Offsets {
+        float X, Y, Z;
+    };
+
+    struct InertialData {
+        Offsets Offset;
+        uint8_t ConfigurationFlag;
+        float ScaleFactor;
+        float Range;
+    };
+
+    struct Set {
+        InertialData Accel;
+        InertialData Gyro;
+    };
+
+    static const Set DefaultSet ={
+        { // ACCEL
+            {
+                24306.08,
+                -3813.78,
+                -30673.64 + 16384.0
+            },
+            0,
+            0,
+            0
+        },
+        { // GYRO
+            {
+                115.81,
+                189.59,
+                -243.07
+            },
+            0,
+            0,
+            0
+        }
+    };
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
 #include "I2Cdev.h"
 #include "MPU6050.h"
-
-constexpr size_t SAMPLES = 10000;
-size_t sample = 0;
+#include "calibration.hpp"
 
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
@@ -15,28 +11,7 @@ MPU6050 accelgyro;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-float sum_ax, sum_ay, sum_az;
-float sum_gx, sum_gy, sum_gz;
-
-/*
-Full-Scale Range
-AFS_SEL=0 ±2 g
-AFS_SEL=1 ±4 g
-AFS_SEL=2 ±8 g
-AFS_SEL=3 ±16 g
-Sensitivity Scale Factor
-AFS_SEL=0 16,384 LSB/g
-AFS_SEL=1 8,192 LSB/g
-AFS_SEL=2 4,096 LSB/g
-AFS_SEL=3 2,048 LSB/g
-*/
-
-constexpr float OFFSET_A_X = 24306.08;
-constexpr float OFFSET_A_Y = -3813.78;
-constexpr float OFFSET_A_Z = -30673.64 + 16384.0;
-constexpr float OFFSET_G_X = 115.81;
-constexpr float OFFSET_G_Y = 189.59;
-constexpr float OFFSET_G_Z = -243.07;
+float roll = 0, pitch = 0, yaw = 0;
 
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -51,45 +26,24 @@ void setup() {
     Serial.println("Initializing I2C devices...");
 
     accelgyro.initialize();
-    // accelgyro.setFullScaleGyroRange();
 
-    // verify connection
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 }
 
 void loop() {
-    // read raw accel/gyro measurements from device
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    sum_ax += ax;
-    sum_ay += ay;
-    sum_az += az;
-    sum_gx += gx;
-    sum_gy += gy;
-    sum_gz += gz;
+    ax -= Calibration::DefaultSet.Accel.Offset.X;
+    ay -= Calibration::DefaultSet.Accel.Offset.Y;
+    az -= Calibration::DefaultSet.Accel.Offset.Z;
+    gx -= Calibration::DefaultSet.Gyro.Offset.X;
+    gy -= Calibration::DefaultSet.Gyro.Offset.Y;
+    gz -= Calibration::DefaultSet.Gyro.Offset.Z;
 
-    if (sample == SAMPLES) {
-        Serial.print("a,g:\t");
-        Serial.print(sum_ax / SAMPLES); Serial.print("\t");
-        Serial.print(sum_ay / SAMPLES); Serial.print("\t");
-        Serial.print(sum_az / SAMPLES); Serial.print("\t");
-        Serial.print(sum_gx / SAMPLES); Serial.print("\t");
-        Serial.print(sum_gy / SAMPLES); Serial.print("\t");
-        Serial.print(sum_gz / SAMPLES); Serial.print("\t");
-        Serial.print("a,g scale:\t");
-        Serial.print(accelgyro.getFullScaleAccelRange()); Serial.print("\t");
-        Serial.print(accelgyro.getFullScaleGyroRange()); Serial.print("\t");
-        Serial.println();
+    roll += (float)gx;
+    pitch += (float)gy;
+    yaw += (float)gz;
 
-        sum_ax = 0;
-        sum_ay = 0;
-        sum_az = 0;
-        sum_gx = 0;
-        sum_gy = 0;
-        sum_gz = 0;
-
-        sample = 0;
-    }
-    sample++;
+    Serial.println(roll);
 }
